7f0f462b92ff468ac64331872dccbbbc8d137af7
MinchoGreenT <dldmstjq99@gmail.com>
2024-03-13 16:03:25 +0900

    initial commit

--- src/main/java/com/modernfarmer/farmusspring/domain/test/TestController.java
+++ src/main/java/com/modernfarmer/farmusspring/domain/test/TestController.java
@@ -1,2 +1,19 @@
-package com.modernfarmer.farmusspring.domain.test;public class TestController {
+package com.modernfarmer.farmusspring.domain.test;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/api/test")
+public class TestController {
+
+    @GetMapping
+    public String test() {
+        return "test";
+    }
 }
