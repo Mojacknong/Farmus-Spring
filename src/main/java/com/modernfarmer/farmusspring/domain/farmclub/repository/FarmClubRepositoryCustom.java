7e5a348e6ad685e5c0dad359153a083599b859e1
MinchoGreenT <dldmstjq99@gmail.com>
2024-04-12 17:48:16 +0900

    [feat] 내 팜클럽 리스트 조회 쿼리 작성

--- src/main/java/com/modernfarmer/farmusspring/domain/farmclub/repository/FarmClubRepositoryCustom.java
+++ src/main/java/com/modernfarmer/farmusspring/domain/farmclub/repository/FarmClubRepositoryCustom.java
@@ -1,5 +1,6 @@
 package com.modernfarmer.farmusspring.domain.farmclub.repository;
 
+import com.modernfarmer.farmusspring.domain.farmclub.dto.res.GetMyFarmClubListResponseDto;
 import com.modernfarmer.farmusspring.domain.farmclub.dto.res.SearchFarmClubResponseDto;
 import com.modernfarmer.farmusspring.domain.farmclub.entity.FarmClub;
 
@@ -7,4 +8,5 @@ import java.util.List;
 
 public interface FarmClubRepositoryCustom {
     List<SearchFarmClubResponseDto> findByConditions(List<String> difficulties, String keyword);
+    List<GetMyFarmClubListResponseDto> findMyFarmClubList(Long userId);
 }
--- src/main/java/com/modernfarmer/farmusspring/domain/farmclub/repository/FarmClubRepositoryImpl.java
+++ src/main/java/com/modernfarmer/farmusspring/domain/farmclub/repository/FarmClubRepositoryImpl.java
@@ -1,13 +1,21 @@
 package com.modernfarmer.farmusspring.domain.farmclub.repository;
 
+import com.modernfarmer.farmusspring.domain.farmclub.dto.res.GetMyFarmClubListResponseDto;
+import com.modernfarmer.farmusspring.domain.farmclub.dto.res.QGetMyFarmClubListResponseDto;
 import com.modernfarmer.farmusspring.domain.farmclub.dto.res.QSearchFarmClubResponseDto;
 import com.modernfarmer.farmusspring.domain.farmclub.dto.res.SearchFarmClubResponseDto;
 import com.modernfarmer.farmusspring.domain.farmclub.entity.QFarmClub;
+import com.modernfarmer.farmusspring.domain.farmclub.entity.QUserFarmClub;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 
 import java.util.List;
+
+import static com.modernfarmer.farmusspring.domain.farmclub.entity.QFarmClub.farmClub;
+import static com.modernfarmer.farmusspring.domain.farmclub.entity.QUserFarmClub.userFarmClub;
+import static com.modernfarmer.farmusspring.domain.myveggiegarden.entity.QMyVeggie.myVeggie;
+import static com.modernfarmer.farmusspring.domain.user.entity.QUser.user;
 
 public class FarmClubRepositoryImpl implements FarmClubRepositoryCustom{
 
@@ -24,6 +32,20 @@ public class FarmClubRepositoryImpl implements FarmClubRepositoryCustom{
                 .from(farmClub)
                 .where(farmClub.difficulty.in(difficulties))
                 .where(farmClub.name.contains(keyword))
+                .fetch();
+    }
+
+    @Override
+    public List<GetMyFarmClubListResponseDto> findMyFarmClubList(Long userId) {
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+
+        return queryFactory
+                .select(new QGetMyFarmClubListResponseDto(farmClub))
+                .from(user)
+                .join(user.myVeggies, myVeggie)
+                .join(myVeggie.userFarmClub, userFarmClub)
+                .join(userFarmClub.farmClub, farmClub)
+                .where(user.id.eq(userId))
                 .fetch();
     }
 }
