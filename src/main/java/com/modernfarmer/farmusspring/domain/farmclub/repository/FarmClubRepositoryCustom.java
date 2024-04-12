f4b807ff7308faad4af2eae06115346f4c76ea32
MinchoGreenT <dldmstjq99@gmail.com>
2024-04-12 23:18:33 +0900

    [feat] 참여중인 팜클럽 정보 조회 쿼리

--- src/main/java/com/modernfarmer/farmusspring/domain/farmclub/repository/FarmClubRepositoryCustom.java
+++ src/main/java/com/modernfarmer/farmusspring/domain/farmclub/repository/FarmClubRepositoryCustom.java
@@ -1,12 +1,13 @@
 package com.modernfarmer.farmusspring.domain.farmclub.repository;
 
 import com.modernfarmer.farmusspring.domain.farmclub.dto.res.GetMyFarmClubListResponseDto;
+import com.modernfarmer.farmusspring.domain.farmclub.vo.GetMyFarmClubVo;
 import com.modernfarmer.farmusspring.domain.farmclub.dto.res.SearchFarmClubResponseDto;
-import com.modernfarmer.farmusspring.domain.farmclub.entity.FarmClub;
 
 import java.util.List;
 
 public interface FarmClubRepositoryCustom {
     List<SearchFarmClubResponseDto> findByConditions(List<String> difficulties, String keyword);
     List<GetMyFarmClubListResponseDto> findMyFarmClubList(Long userId);
+    GetMyFarmClubVo findMyFarmClub(Long farmClubId, Long userId);
 }
--- src/main/java/com/modernfarmer/farmusspring/domain/farmclub/repository/FarmClubRepositoryImpl.java
+++ src/main/java/com/modernfarmer/farmusspring/domain/farmclub/repository/FarmClubRepositoryImpl.java
@@ -1,11 +1,10 @@
 package com.modernfarmer.farmusspring.domain.farmclub.repository;
 
-import com.modernfarmer.farmusspring.domain.farmclub.dto.res.GetMyFarmClubListResponseDto;
-import com.modernfarmer.farmusspring.domain.farmclub.dto.res.QGetMyFarmClubListResponseDto;
-import com.modernfarmer.farmusspring.domain.farmclub.dto.res.QSearchFarmClubResponseDto;
-import com.modernfarmer.farmusspring.domain.farmclub.dto.res.SearchFarmClubResponseDto;
+import com.modernfarmer.farmusspring.domain.farmclub.dto.res.*;
 import com.modernfarmer.farmusspring.domain.farmclub.entity.QFarmClub;
-import com.modernfarmer.farmusspring.domain.farmclub.entity.QUserFarmClub;
+import com.modernfarmer.farmusspring.domain.farmclub.exception.custom.FarmClubNotFoundException;
+import com.modernfarmer.farmusspring.domain.farmclub.vo.GetMyFarmClubVo;
+import com.modernfarmer.farmusspring.domain.farmclub.vo.QGetMyFarmClubVo_BaseInfo;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -17,7 +16,7 @@ import static com.modernfarmer.farmusspring.domain.farmclub.entity.QUserFarmClub
 import static com.modernfarmer.farmusspring.domain.myveggiegarden.entity.QMyVeggie.myVeggie;
 import static com.modernfarmer.farmusspring.domain.user.entity.QUser.user;
 
-public class FarmClubRepositoryImpl implements FarmClubRepositoryCustom{
+public class FarmClubRepositoryImpl implements FarmClubRepositoryCustom {
 
     @PersistenceContext
     private EntityManager em;
@@ -48,4 +47,35 @@ public class FarmClubRepositoryImpl implements FarmClubRepositoryCustom{
                 .where(user.id.eq(userId))
                 .fetch();
     }
+
+    @Override
+    public GetMyFarmClubVo findMyFarmClub(Long farmClubId, Long userId) {
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+
+        GetMyFarmClubVo.BaseInfo baseInfo = queryFactory
+                .select(new QGetMyFarmClubVo_BaseInfo(farmClub))
+                .from(farmClub)
+                .where(farmClub.id.eq(farmClubId))
+                .fetchOne();
+
+        if (baseInfo != null) {
+            Long userFarmClubCount = queryFactory
+                    .select(userFarmClub.count())
+                    .from(userFarmClub)
+                    .where(userFarmClub.farmClub.id.eq(farmClubId))
+                    .fetchOne();
+
+            Integer daySinceStart = queryFactory
+                    .select(userFarmClub.createdDate.dayOfYear().subtract(farmClub.startedAt.dayOfYear()))
+                    .from(userFarmClub)
+                    .where(userFarmClub.farmClub.id.eq(farmClubId))
+                    .where(userFarmClub.userId.eq(userId))
+                    .fetchOne();
+
+            return GetMyFarmClubVo.of(baseInfo, userFarmClubCount, daySinceStart);
+        } else {
+            throw new FarmClubNotFoundException("내 팜클럽을 불러오는 도중 에러가 발생했습니다.");
+        }
+    }
+
 }
