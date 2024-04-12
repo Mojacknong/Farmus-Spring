becc7bb8b57d79c0024444182bf12a030fdc6e9e
MinchoGreenT <dldmstjq99@gmail.com>
2024-04-12 16:35:07 +0900

    [feat] VeggieInfo Helper 적용 및 준비사항 조회 메서드 추가

---
+++ src/main/java/com/modernfarmer/farmusspring/domain/veggieinfo/helper/VeggieInfoHelper.java
@@ -0,0 +1,26 @@
+package com.modernfarmer.farmusspring.domain.veggieinfo.helper;
+
+import com.modernfarmer.farmusspring.domain.veggieinfo.entity.VeggieInfo;
+import com.modernfarmer.farmusspring.domain.veggieinfo.exception.custom.VeggieInfoNotFoundException;
+import com.modernfarmer.farmusspring.domain.veggieinfo.repository.VeggieInfoRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.bson.types.ObjectId;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class VeggieInfoHelper {
+
+    private final VeggieInfoRepository veggieInfoRepository;
+
+    public List<String> getVeggieInfoHelp(String veggieInfoId) {
+        VeggieInfo veggieInfo = veggieInfoRepository.findById(new ObjectId(veggieInfoId))
+                .orElseThrow(() -> new VeggieInfoNotFoundException("존재하지 않는 채소 정보 아이디입니다."));
+
+        return veggieInfo.getHelp();
+    }
+}
