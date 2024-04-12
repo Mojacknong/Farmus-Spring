28ab79608c657a34caced35a55b8b0dbfb0ccfd6
MinchoGreenT <dldmstjq99@gmail.com>
2024-04-12 16:35:37 +0900

    [feat] 팜클럽 탐색 단일조회 응답 Dto 매핑 메서드 추가

--- src/main/java/com/modernfarmer/farmusspring/domain/farmclub/dto/res/GetFarmClubResponseDto.java
+++ src/main/java/com/modernfarmer/farmusspring/domain/farmclub/dto/res/GetFarmClubResponseDto.java
@@ -1,5 +1,9 @@
 package com.modernfarmer.farmusspring.domain.farmclub.dto.res;
 
+import com.modernfarmer.farmusspring.domain.farmclub.entity.FarmClub;
+
+import java.util.List;
+
 public record GetFarmClubResponseDto (
         Long farmClubId,
         String farmClubName,
@@ -9,9 +13,19 @@ public record GetFarmClubResponseDto (
         String startedAt,
         int maxMemberCount,
         int currentMemberCount,
-        String preparation
+        List<String> help
 ){
-    public static GetFarmClubResponseDto of(Long farmClubId, String farmClubName, String farmClubDescription, String veggieName, String veggieImage, String startedAt, int maxMemberCount, int currentMemberCount, String preparation) {
-        return new GetFarmClubResponseDto(farmClubId, farmClubName, farmClubDescription, veggieName, veggieImage, startedAt, maxMemberCount, currentMemberCount, preparation);
+    public static GetFarmClubResponseDto of(FarmClub farmClub, int currentMemberCount, List<String> help) {
+        return new GetFarmClubResponseDto(
+                farmClub.getId(),
+                farmClub.getName(),
+                farmClub.getDescription(),
+                farmClub.getVeggieName(),
+                farmClub.getVeggieImage(),
+                farmClub.getStartedAt().toString(),
+                farmClub.getMaxUser(),
+                currentMemberCount,
+                help
+        );
     }
 }
