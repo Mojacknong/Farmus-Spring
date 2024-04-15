b08130603e13bd82351985c52dfd861a7ce25fc7
MinchoGreenT <dldmstjq99@gmail.com>
2024-04-15 20:06:37 +0900

    [feat] 미션 인증 API 추가

--- src/main/java/com/modernfarmer/farmusspring/domain/farmclub/controller/FarmClubController.java
+++ src/main/java/com/modernfarmer/farmusspring/domain/farmclub/controller/FarmClubController.java
@@ -2,13 +2,16 @@ package com.modernfarmer.farmusspring.domain.farmclub.controller;
 
 import com.modernfarmer.farmusspring.domain.auth.entity.CustomUser;
 import com.modernfarmer.farmusspring.domain.farmclub.dto.req.CreateFarmClubRequestDto;
+import com.modernfarmer.farmusspring.domain.farmclub.dto.req.CreateMissionPostRequestDto;
 import com.modernfarmer.farmusspring.domain.farmclub.helper.FarmClubHelper;
 import com.modernfarmer.farmusspring.domain.farmclub.service.FarmClubService;
+import com.modernfarmer.farmusspring.domain.farmclub.service.MissionPostService;
 import com.modernfarmer.farmusspring.global.response.BaseResponseDto;
 import com.modernfarmer.farmusspring.global.response.SuccessCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -18,7 +21,7 @@ import java.util.List;
 public class FarmClubController {
 
     private final FarmClubService farmClubService;
-    private final FarmClubHelper farmClubHelper;
+    private final MissionPostService missionPostService;
 
     // 요청 : 이름, 설명, 최대인원, 모집기한, 내 채소 id, 채소정보 id
     // 응답 : 팜클럽 id
@@ -41,7 +44,7 @@ public class FarmClubController {
             @RequestParam List<String> difficulties,
             @RequestParam String keyword
     ) {
-        return BaseResponseDto.of(SuccessCode.SUCCESS, farmClubHelper.searchFarmClub(difficulties, keyword));
+        return BaseResponseDto.of(SuccessCode.SUCCESS, farmClubService.searchFarmClub(difficulties, keyword));
     }
 
     @GetMapping("/{id}")
@@ -64,5 +67,13 @@ public class FarmClubController {
             @AuthenticationPrincipal CustomUser user
     ) {
         return BaseResponseDto.of(SuccessCode.SUCCESS, farmClubService.getMyFarmClub(farmClubId, user.getUserId()));
+    }
+
+    @PostMapping("/mission")
+    public BaseResponseDto<?> createMissionPost(
+            @RequestPart CreateMissionPostRequestDto requestDto,
+            @RequestPart(value = "image") MultipartFile image
+    ) {
+        return BaseResponseDto.of(SuccessCode.CREATED, missionPostService.createMissionPost(requestDto, image));
     }
 }
