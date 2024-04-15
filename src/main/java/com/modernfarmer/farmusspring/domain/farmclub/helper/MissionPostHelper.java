47b6753b76bdc83212b3777172b8014a492a597b
MinchoGreenT <dldmstjq99@gmail.com>
2024-04-15 20:05:09 +0900

    [feat] MissionPost Helper 클래스 생성

---
+++ src/main/java/com/modernfarmer/farmusspring/domain/farmclub/helper/MissionPostHelper.java
@@ -0,0 +1,9 @@
+package com.modernfarmer.farmusspring.domain.farmclub.helper;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MissionPostHelper {
+}
