d369fcbbc25e8a62846028d3a39bec48d11b4a40
MinchoGreenT <dldmstjq99@gmail.com>
2024-04-15 20:05:59 +0900

    [fix] 기존 예외 클래스 삭제

--- src/main/java/com/modernfarmer/farmusspring/domain/farmclub/exception/custom/FarmClubNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.modernfarmer.farmusspring.domain.farmclub.exception.custom;
-
-public class FarmClubNotFoundException extends RuntimeException {
-
-    public FarmClubNotFoundException(String message) {
-        super(message);
-    }
-}
