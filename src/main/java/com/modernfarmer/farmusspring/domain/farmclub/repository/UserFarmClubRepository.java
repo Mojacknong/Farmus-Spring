ab0f72ffb260e2df443af3e1ffaa8dad5e041828
MinchoGreenT <dldmstjq99@gmail.com>
2024-04-15 20:04:41 +0900

    [feat] UserFarmClub 기본 클래스 생성 및 조회 기능 구현

---
+++ src/main/java/com/modernfarmer/farmusspring/domain/farmclub/repository/UserFarmClubRepository.java
@@ -0,0 +1,7 @@
+package com.modernfarmer.farmusspring.domain.farmclub.repository;
+
+import com.modernfarmer.farmusspring.domain.farmclub.entity.UserFarmClub;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserFarmClubRepository extends JpaRepository<UserFarmClub, Long> {
+}
