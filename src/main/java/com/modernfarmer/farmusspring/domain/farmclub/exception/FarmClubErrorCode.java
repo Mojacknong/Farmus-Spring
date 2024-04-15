8240fd907a3e6d5b26ce2107fc9ee20fdabd28f9
MinchoGreenT <dldmstjq99@gmail.com>
2024-04-15 20:06:22 +0900

    [feat] 등록 정보 조회 에러 추가

--- src/main/java/com/modernfarmer/farmusspring/domain/farmclub/exception/FarmClubErrorCode.java
+++ src/main/java/com/modernfarmer/farmusspring/domain/farmclub/exception/FarmClubErrorCode.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum FarmClubErrorCode implements ResponseCode {
 
-    FARM_CLUB_NOT_FOUND(3001, "해당 팜클럽을 찾을 수 없습니다.");
+    FARM_CLUB_NOT_FOUND(3001, "해당 팜클럽을 찾을 수 없습니다."),
+    USER_FARM_CLUB_NOT_FOUND(3002, "해당 등록 정보를 찾을 수 없습니다.");
 
     private final int code;
     private final String message;
