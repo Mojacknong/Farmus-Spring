bb8ce6f37424146fc08f5de843c867830c75dbae
MinchoGreenT <dldmstjq99@gmail.com>
2024-05-06 20:13:06 +0900

    [feat] helper에 미션포스트 조회 메서드 추가

--- src/main/java/com/modernfarmer/farmusspring/domain/farmclub/helper/MissionPostHelper.java
+++ src/main/java/com/modernfarmer/farmusspring/domain/farmclub/helper/MissionPostHelper.java
@@ -1,9 +1,20 @@
 package com.modernfarmer.farmusspring.domain.farmclub.helper;
 
+import com.modernfarmer.farmusspring.domain.farmclub.entity.MissionPost;
+import com.modernfarmer.farmusspring.domain.farmclub.exception.FarmClubErrorCode;
+import com.modernfarmer.farmusspring.domain.farmclub.exception.custom.FarmClubEntityNotFoundException;
+import com.modernfarmer.farmusspring.domain.farmclub.repository.MissionPostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class MissionPostHelper {
+
+    private final MissionPostRepository missionPostRepository;
+
+    public MissionPost getMissionPost(Long missionPostId) {
+        return missionPostRepository.findById(missionPostId)
+                .orElseThrow(() -> new FarmClubEntityNotFoundException("존재하지 않는 미션 게시글입니다.", FarmClubErrorCode.MISSION_POST_NOT_FOUND));
+    }
 }
