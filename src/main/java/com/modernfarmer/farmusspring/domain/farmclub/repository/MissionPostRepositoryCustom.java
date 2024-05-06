701fa245c47b7d3f3aa77fd72f1e52beafe93217
MinchoGreenT <dldmstjq99@gmail.com>
2024-05-06 20:12:44 +0900

    [feat] MissionPost 목록조회, 댓글조회 쿼리

--- src/main/java/com/modernfarmer/farmusspring/domain/farmclub/repository/MissionPostRepositoryCustom.java
+++ src/main/java/com/modernfarmer/farmusspring/domain/farmclub/repository/MissionPostRepositoryCustom.java
@@ -2,10 +2,14 @@ package com.modernfarmer.farmusspring.domain.farmclub.repository;
 
 
 import com.modernfarmer.farmusspring.domain.farmclub.vo.GetMissionPostListWithStepCountsAndImagesVo;
+import com.modernfarmer.farmusspring.domain.farmclub.vo.MissionPostCommentVo;
+import com.modernfarmer.farmusspring.domain.farmclub.vo.MissionPostVo;
 
 import java.util.List;
 
 public interface MissionPostRepositoryCustom {
 
     List<GetMissionPostListWithStepCountsAndImagesVo> getMissionPostStepNumAndImage(Long farmClubId);
+    List<MissionPostCommentVo> getMissionPostComment(Long missionPostId);
+    List<MissionPostVo> getMissionPostList(Long missionPostId);
 }
