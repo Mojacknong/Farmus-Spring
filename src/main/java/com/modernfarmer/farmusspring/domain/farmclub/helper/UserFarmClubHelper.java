7627fe87a81f387e4d98923ae7b053fee3b3bdc9
MinchoGreenT <dldmstjq99@gmail.com>
2024-05-06 20:10:22 +0900

    [fix] 예외처리 변경

--- src/main/java/com/modernfarmer/farmusspring/domain/farmclub/helper/UserFarmClubHelper.java
+++ src/main/java/com/modernfarmer/farmusspring/domain/farmclub/helper/UserFarmClubHelper.java
@@ -2,7 +2,7 @@ package com.modernfarmer.farmusspring.domain.farmclub.helper;
 
 import com.modernfarmer.farmusspring.domain.farmclub.entity.UserFarmClub;
 import com.modernfarmer.farmusspring.domain.farmclub.exception.FarmClubErrorCode;
-import com.modernfarmer.farmusspring.domain.farmclub.exception.custom.EntityNotFoundException;
+import com.modernfarmer.farmusspring.domain.farmclub.exception.custom.FarmClubEntityNotFoundException;
 import com.modernfarmer.farmusspring.domain.farmclub.repository.UserFarmClubRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +17,6 @@ public class UserFarmClubHelper {
 
     public UserFarmClub getUserFarmClubEntity(Long id) {
         return userFarmClubRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 등록 정보입니다.", FarmClubErrorCode.USER_FARM_CLUB_NOT_FOUND));
+                .orElseThrow(() -> new FarmClubEntityNotFoundException("존재하지 않는 등록 정보입니다.", FarmClubErrorCode.USER_FARM_CLUB_NOT_FOUND));
     }
 }
