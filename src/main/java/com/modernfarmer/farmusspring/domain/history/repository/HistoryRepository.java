6009f0b2ecaa99ac376e6261481d9ef19341e7bb
MinchoGreenT <dldmstjq99@gmail.com>
2024-05-12 00:31:55 +0900

    [fix] 유저 아이디로 히스토리 조회 메서드

--- src/main/java/com/modernfarmer/farmusspring/domain/history/repository/HistoryRepository.java
+++ src/main/java/com/modernfarmer/farmusspring/domain/history/repository/HistoryRepository.java
@@ -4,5 +4,9 @@ import com.modernfarmer.farmusspring.domain.history.document.History;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface HistoryRepository extends MongoRepository<History, ObjectId>, CustomHistoryRepository {
+
+    Optional<History> findByUserId(Long userId);
 }
