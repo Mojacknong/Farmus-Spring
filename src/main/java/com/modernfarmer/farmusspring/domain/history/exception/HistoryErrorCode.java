80d2fe6338b1f69b35e03de677f31f99bf69fa5d
MinchoGreenT <dldmstjq99@gmail.com>
2024-05-12 00:31:11 +0900

    [fix] 예외 코드명 ENTITY_NOT_FOUND로 변경

--- src/main/java/com/modernfarmer/farmusspring/domain/history/exception/HistoryErrorCode.java
+++ src/main/java/com/modernfarmer/farmusspring/domain/history/exception/HistoryErrorCode.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum HistoryErrorCode implements ResponseCode {
 
-    HISTORY_NOT_FOUND(6001, "히스토리 정보를 찾을 수 없습니다.");
+    ENTITY_NOT_FOUND(6001, "엔티티를 찾을 수 없습니다.");
 
     private final int code;
     private final String message;
