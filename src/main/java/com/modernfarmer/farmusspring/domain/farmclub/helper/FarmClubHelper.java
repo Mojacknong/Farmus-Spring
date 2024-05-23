ebbf53443c0d843c10a93e5cc7fc00ff070d4bc3
MinchoGreenT <dldmstjq99@gmail.com>
2024-05-24 06:01:51 +0900

    [fix] 팜클럽 조회 예외처리 수정

--- src/main/java/com/modernfarmer/farmusspring/domain/farmclub/helper/FarmClubHelper.java
+++ src/main/java/com/modernfarmer/farmusspring/domain/farmclub/helper/FarmClubHelper.java
@@ -2,6 +2,8 @@ package com.modernfarmer.farmusspring.domain.farmclub.helper;
 
 import com.modernfarmer.farmusspring.domain.farmclub.dto.res.SearchFarmClubResponseDto;
 import com.modernfarmer.farmusspring.domain.farmclub.entity.FarmClub;
+import com.modernfarmer.farmusspring.domain.farmclub.exception.FarmClubErrorCode;
+import com.modernfarmer.farmusspring.domain.farmclub.exception.custom.FarmClubEntityNotFoundException;
 import com.modernfarmer.farmusspring.domain.farmclub.repository.FarmClubRepository;
 import com.modernfarmer.farmusspring.domain.history.vo.HistoryDetailVo;
 import lombok.RequiredArgsConstructor;
@@ -18,7 +20,8 @@ public class FarmClubHelper {
     private final FarmClubRepository farmClubRepository;
 
     public FarmClub getFarmClubEntity(Long id) {
-        return farmClubRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 팜클럽입니다."));
+        return farmClubRepository.findById(id).orElseThrow(() ->
+                new FarmClubEntityNotFoundException("존재하지 않는 팜클럽입니다.", FarmClubErrorCode.FARM_CLUB_NOT_FOUND));
     }
 
     public HistoryDetailVo getFarmClubDetail(Long userFarmClubId) {
