c0350537a9fa595853bcaca1262e99902280e5d2
MinchoGreenT <dldmstjq99@gmail.com>
2024-05-24 06:04:59 +0900

    [fix] 내 채소 찾기 메서드

--- src/main/java/com/modernfarmer/farmusspring/domain/myveggiegarden/helper/MyVeggieHelper.java
+++ src/main/java/com/modernfarmer/farmusspring/domain/myveggiegarden/helper/MyVeggieHelper.java
@@ -1,5 +1,8 @@
 package com.modernfarmer.farmusspring.domain.myveggiegarden.helper;
 
+import com.modernfarmer.farmusspring.domain.myveggiegarden.entity.MyVeggie;
+import com.modernfarmer.farmusspring.domain.myveggiegarden.exception.MyVeggieGardenErrorCode;
+import com.modernfarmer.farmusspring.domain.myveggiegarden.exception.custom.MyVeggieGardenBaseException;
 import com.modernfarmer.farmusspring.domain.myveggiegarden.repository.MyVeggieRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,4 +14,9 @@ import org.springframework.stereotype.Component;
 public class MyVeggieHelper {
 
     private final MyVeggieRepository myVeggieRepository;
+
+    public MyVeggie getMyVeggieEntity(Long id) {
+        return myVeggieRepository.findById(id).orElseThrow(() ->
+                new MyVeggieGardenBaseException("존재하지 않는 나의 채소입니다.", MyVeggieGardenErrorCode.NOT_FOUND_VEGGIE));
+    }
 }
