8274cedae959631337c6fbef2bf5b86bff495d7d
MinchoGreenT <dldmstjq99@gmail.com>
2024-06-04 10:29:17 +0900

    [fix] 채소 정보 내 id값 json 프로퍼티 수정

--- src/main/java/com/modernfarmer/farmusspring/domain/veggieinfo/vo/InfoForRegisterVo.java
+++ src/main/java/com/modernfarmer/farmusspring/domain/veggieinfo/vo/InfoForRegisterVo.java
@@ -1,9 +1,12 @@
 package com.modernfarmer.farmusspring.domain.veggieinfo.vo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.modernfarmer.farmusspring.domain.veggieinfo.enums.Difficulty;
 
 
 public record InfoForRegisterVo(
+
+    @JsonProperty("id")
     String _id,
     String name,
     Difficulty difficulty,
