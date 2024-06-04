c87594490d865f55438e843915a9ad84e7610909
MinchoGreenT <dldmstjq99@gmail.com>
2024-06-04 11:04:29 +0900

    [fix] 채소 정보 반환 시 난이도 순서로 정렬

--- src/main/java/com/modernfarmer/farmusspring/domain/veggieinfo/service/VeggieInfoService.java
+++ src/main/java/com/modernfarmer/farmusspring/domain/veggieinfo/service/VeggieInfoService.java
@@ -3,6 +3,7 @@ package com.modernfarmer.farmusspring.domain.veggieinfo.service;
 import com.modernfarmer.farmusspring.domain.user.helper.UserHelper;
 import com.modernfarmer.farmusspring.domain.veggieinfo.dto.res.GetRecommendVeggieDto;
 import com.modernfarmer.farmusspring.domain.veggieinfo.dto.res.GetVeggieListResponseDto;
+import com.modernfarmer.farmusspring.domain.veggieinfo.enums.Difficulty;
 import com.modernfarmer.farmusspring.domain.veggieinfo.vo.InfoForRegisterVo;
 import com.modernfarmer.farmusspring.domain.veggieinfo.entity.VeggieInfo;
 import com.modernfarmer.farmusspring.domain.veggieinfo.repository.VeggieInfoRepository;
@@ -20,8 +21,9 @@ public class VeggieInfoService {
     private final UserHelper userHelper;
 
     public GetVeggieListResponseDto getVeggieInfoListForRegister() {
-        // Get all veggie info list for register
-        return GetVeggieListResponseDto.of(veggieInfoRepository.getVeggieInfoListForRegister());
+        List<InfoForRegisterVo> veggieList = veggieInfoRepository.getVeggieInfoListForRegister();
+        sortByDifficulty(veggieList);
+        return GetVeggieListResponseDto.of(veggieList);
     }
 
     public String getFirstStepName(String veggieInfoId) {
@@ -33,5 +35,21 @@ public class VeggieInfoService {
     public List<GetRecommendVeggieDto> getRecommendVeggieList(Long userid) {
         String difficulty = userHelper.getUserLevel(userid);
         return veggieInfoRepository.getRecommendVeggieList(difficulty);
+    }
+
+    private static void sortByDifficulty(List<InfoForRegisterVo> veggieList) {
+        veggieList.sort((a, b) -> {
+            if (a.difficulty().equals(Difficulty.EASY)) {
+                return -1;
+            } else if (a.difficulty().equals(Difficulty.NORMAL)) {
+                if (b.difficulty().equals(Difficulty.EASY)) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            } else {
+                return 1;
+            }
+        });
     }
 }
