fb387a252e9bf4eccf487bef20ed0f844de9ac94
seungryeol <opop0421@naver.com>
2024-07-17 02:06:20 +0900

    [feat] 루틴 수정 API 요청 dto

---
+++ src/main/java/com/modernfarmer/farmusspring/domain/myveggiegarden/dto/request/RoutineUpdate.java
@@ -0,0 +1,22 @@
+package com.modernfarmer.farmusspring.domain.myveggiegarden.dto.request;
+
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoutineUpdate {
+
+    @NotNull(message = "null 값을 가지면 안됩니다.")
+    private String content;
+
+
+    private int period;
+
+    @NotNull(message = "null 값을 가지면 안됩니다.")
+    private Long routineId;
+}
