775b8cdadf2dc6c324a8129ec04c398de6b15c54
seungryeol <opop0421@naver.com>
2024-07-17 02:49:30 +0900

    [feat] 루틴 삭제 요청 dto

---
+++ src/main/java/com/modernfarmer/farmusspring/domain/myveggiegarden/dto/request/RoutineDelete.java
@@ -0,0 +1,16 @@
+package com.modernfarmer.farmusspring.domain.myveggiegarden.dto.request;
+
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoutineDelete {
+
+    @NotNull(message = "null 값을 가지면 안됩니다.")
+    private Long routineId;
+}
