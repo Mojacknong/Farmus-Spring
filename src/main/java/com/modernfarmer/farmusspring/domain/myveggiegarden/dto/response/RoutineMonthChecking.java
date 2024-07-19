7902840313979c80011166a96f68e61577274c4e
seungryeol <opop0421@naver.com>
2024-07-20 03:56:20 +0900

    [feat] 달 별 달력 체킹 조회 dto API

---
+++ src/main/java/com/modernfarmer/farmusspring/domain/myveggiegarden/dto/response/RoutineMonthChecking.java
@@ -0,0 +1,29 @@
+package com.modernfarmer.farmusspring.domain.myveggiegarden.dto.response;
+
+import com.modernfarmer.farmusspring.domain.myveggiegarden.entity.MyVeggie;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Date;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Slf4j
+public class RoutineMonthChecking {
+
+
+    private List<Date> date;
+
+
+    public static RoutineMonthChecking of(List<Date> date){
+        return new RoutineMonthChecking(date);
+    }
+
+
+
+
+}
