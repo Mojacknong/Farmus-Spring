122431f678b53ef3501d502f1fc7b6f226777fe7
MinchoGreenT <dldmstjq99@gmail.com>
2024-07-25 14:35:07 +0900

    [feat] 채소/팜클럽 히스토리 목록, 채소/팜클럽 히스토리 디테일 조회 기능 구현

--- src/main/java/com/modernfarmer/farmusspring/domain/history/service/HistoryService.java
+++ src/main/java/com/modernfarmer/farmusspring/domain/history/service/HistoryService.java
@@ -1,7 +1,9 @@
 package com.modernfarmer.farmusspring.domain.history.service;
 
 import com.modernfarmer.farmusspring.domain.history.document.History;
-import com.modernfarmer.farmusspring.domain.history.dto.res.HistoryResponseDto;
+import com.modernfarmer.farmusspring.domain.history.document.HistoryFarmClubDetail;
+import com.modernfarmer.farmusspring.domain.history.document.HistoryVeggieDetail;
+import com.modernfarmer.farmusspring.domain.history.dto.res.*;
 import com.modernfarmer.farmusspring.domain.history.helper.HistoryHelper;
 import com.modernfarmer.farmusspring.domain.history.repository.HistoryRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,7 +19,26 @@ public class HistoryService {
     private final HistoryHelper historyHelper;
 
     public HistoryResponseDto getUserHistory(Long userId) {
+        return null;
+    }
+
+    public VeggieHistoryDetailResponseDto getVeggieHistoryDetail(ObjectId detailId) {
+        HistoryVeggieDetail historyVeggieDetail = historyHelper.getVeggieHistoryDetail(detailId);
+        return VeggieHistoryDetailResponseDto.of(historyVeggieDetail.getDiaryPosts(), historyVeggieDetail.getFarmResult());
+    }
+
+    public VeggieHistoryListResponseDto getVeggieHistories(Long userId) {
         History history = historyHelper.getUserHistory(userId);
-        return HistoryResponseDto.of(history);
+        return VeggieHistoryListResponseDto.of(history.getVeggieHistoryDetails());
+    }
+
+    public FarmClubHistoryDetailResponseDto getFarmClubHistoryDetail(ObjectId detailId) {
+        HistoryFarmClubDetail historyFarmClubDetail = historyHelper.getFarmClubHistoryDetail(detailId);
+        return FarmClubHistoryDetailResponseDto.of(historyFarmClubDetail.getMissionPostList());
+    }
+
+    public FarmClubHistoryListResponseDto getFarmClubHistories(Long userId) {
+        History history = historyHelper.getUserHistory(userId);
+        return FarmClubHistoryListResponseDto.of(history.getFarmClubHistoryDetails());
     }
 }
