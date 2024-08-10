cc1879516d2f982f272f1578cdf2e032d97d659b
seungryeol <opop0421@naver.com>
2024-08-11 00:52:34 +0900

    [feat] DiaryLike Repository 생성 후 좋아요 개수 함수 구현

---
+++ src/main/java/com/modernfarmer/farmusspring/domain/myveggiegarden/repository/DiaryLikeRepository.java
@@ -0,0 +1,20 @@
+package com.modernfarmer.farmusspring.domain.myveggiegarden.repository;
+
+import com.modernfarmer.farmusspring.domain.myveggiegarden.entity.Diary;
+import com.modernfarmer.farmusspring.domain.myveggiegarden.entity.DiaryComment;
+import com.modernfarmer.farmusspring.domain.myveggiegarden.entity.DiaryLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface DiaryLikeRepository extends JpaRepository<DiaryLike, Long> {
+
+    @Query("SELECT count(dl) FROM diary_like as dl WHERE dl.id = :diaryId ")
+    int findDiaryLikeCountById(@Param("diaryId") Long diaryId);
+
+}
