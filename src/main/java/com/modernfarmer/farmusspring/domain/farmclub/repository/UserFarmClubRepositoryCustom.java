518667f10da3a5f0ad4135a888f51eda9ca8842e
seungryeol <opop0421@naver.com>
2024-08-26 11:19:34 +0900

    [feat] 유저의 팜클럽 도메인 정보 삭제

--- src/main/java/com/modernfarmer/farmusspring/domain/farmclub/repository/UserFarmClubRepositoryCustom.java
+++ src/main/java/com/modernfarmer/farmusspring/domain/farmclub/repository/UserFarmClubRepositoryCustom.java
@@ -2,6 +2,7 @@ package com.modernfarmer.farmusspring.domain.farmclub.repository;
 
 import com.modernfarmer.farmusspring.domain.farmclub.vo.SuccessFarmClubVo;
 import com.modernfarmer.farmusspring.domain.farmclub.entity.UserFarmClub;
+import com.modernfarmer.farmusspring.domain.myveggiegarden.entity.MyVeggie;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,4 +13,6 @@ public interface UserFarmClubRepositoryCustom {
 
     List<Long> findFarmClubIdsByUserId(Long userId);
     SuccessFarmClubVo getFarmClubRecord(Long userId, Long farmClubId);
+
+    void deleteFarmClubDomain(List<MyVeggie> myVeggieList);
 }
