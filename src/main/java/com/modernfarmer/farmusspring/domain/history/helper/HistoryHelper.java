4e6bae59b8f55bdd449f43d8b1d349f4a8ea60c5
seungryeol <opop0421@naver.com>
2024-09-03 20:40:12 +0900

    [fix] 히스토리 삭제 함수 변경 -> 값이 있으면 삭제, 없으면 리턴

--- src/main/java/com/modernfarmer/farmusspring/domain/history/helper/HistoryHelper.java
+++ src/main/java/com/modernfarmer/farmusspring/domain/history/helper/HistoryHelper.java
@@ -27,6 +27,7 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import static com.modernfarmer.farmusspring.domain.history.utils.StringUtil.getHistoryPeriod;
 import static com.modernfarmer.farmusspring.domain.history.utils.StringUtil.getPostFormattedDate;
@@ -51,8 +52,8 @@ public class HistoryHelper {
     }
 
     public void deleteHistory(Long userId) {
-        History history = getUserHistory(userId);
-        historyRepository.delete(history);
+        Optional<History> historyOptional = historyRepository.findByUserId(userId);
+        historyOptional.ifPresent(historyRepository::delete);
     }
 
     public String createFarmClubHistoryDetail(Long userId, Long userFarmClubId, String veggieInfoId) {
